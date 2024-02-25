b52213cc6051cd6fd040cd81bef024dac25a8c25
amin hadadi <amin.haddadi1995@gmail.com>
2024-02-25 17:35:35 +0330

    complete main page with images

--- my-app/src/app/(root)/page.tsx
+++ my-app/src/app/(root)/page.tsx
@@ -1,5 +1,51 @@
-const Home = () => {
-	return <>home</>;
+import { GridView } from "@mui/icons-material";
+import Slider from "react-slick";
+import { Grid } from "@mui/material";
+import Image from "next/image";
+
+interface Product {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+	rating: { rate: number; count: number };
+}
+
+const Home = async () => {
+	const res = await fetch("https://fakestoreapi.com/products", {
+		cache: "no-cache",
+	});
+	if (!res.ok) {
+		throw new Error("HTTP error! status:");
+	}
+	const p = await res.json();
+	return (
+		<>
+			{p.map((pro: Product, i: number) => (
+				<Image key={i} src={pro.image} width={200} height={200} alt="prod" />
+			))}
+			{/* <Slider>
+				{p?.map((pro: Product, i: number) => (
+					<Image key={i} src={pro.image} width={200} height={200} alt="prod" />
+				))}
+			</Slider> */}
+			{/* <GridView>
+				{p.map((pro: Product, i: number) => (
+					<Grid item key={i} xs={1}>
+						<Image
+							key={i}
+							src={pro.image}
+							width={200}
+							height={200}
+							alt="prod"
+						/>
+					</Grid>
+				))}
+			</GridView> */}
+		</>
+	);
 };
 
 export default Home;
